81f29ba05909fb599e682a5c0171cf019463781c
Mr Ezak <ezaksaeed@gmail.com>
2019-05-20 02:12:29 +0430

    First try to fix the header.

--- components/nobles/MyLayout.js
+++ components/nobles/MyLayout.js
@@ -1,7 +1,17 @@
+import { createGlobalStyle } from 'styled-components';
 import Header from './Header'
+
+const GlobalStyle = createGlobalStyle`
+    body{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+`;
 
 const Layout = props => (
     <div>
+        <GlobalStyle />
         <Header />
         {props.children}
     </div>
