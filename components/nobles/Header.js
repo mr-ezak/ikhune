0862490bb610317d5d0951f6de1fa765964f34f6
Mr Ezak <ezaksaeed@gmail.com>
2019-05-23 00:41:15 +0430

    Finish the header appearance

--- components/nobles/Header.js
+++ components/nobles/Header.js
@@ -1,21 +1,39 @@
 import Link from "next/link"
 import styled from "styled-components"
+import { BarsIcon, NotifiIcon } from "./Icons"
 
 export const Header = () => (
   <StyledHeader>
-    <HeaderNotification />
+    <HeaderNotification>
+      <NotifiIcon />
+    </HeaderNotification>
+    <HeaderBars>
+      <BarsIcon />
+    </HeaderBars>
   </StyledHeader>
 )
 
 const StyledHeader = styled.div`
   width: 100%;
   height: 50px;
-  background: gray;
+  background: #e040fb;
   text-align: center;
 `
 const HeaderNotification = styled.div`
   float: left;
   width: 50px;
   height: 50px;
-  background: red;
+
+  svg {
+    margin: 10px auto;
+  }
+`
+const HeaderBars = styled.div`
+  float: right;
+  width: 50px;
+  height: 50px;
+
+  svg {
+    margin: 10px auto;
+  }
 `
---
+++ components/nobles/Icons.js
@@ -0,0 +1,45 @@
+import React from "react"
+
+export const BarsIcon = props => (
+  <svg width="2em" height="2em" viewBox="0 0 192 192" {...props}>
+    <g
+      fill="none"
+      strokeMiterlimit={10}
+      fontFamily="none"
+      fontWeight="none"
+      fontSize="none"
+      textAnchor="none"
+      style={{
+        mixBlendMode: "normal"
+      }}
+    >
+      <path d="M0 192V0h192v192z" />
+      <path
+        d="M0 28.8V48h192V28.8zm0 57.6v19.2h192V86.4zM0 144v19.2h192V144z"
+        fill="#fff"
+      />
+    </g>
+  </svg>
+)
+
+export const NotifiIcon = props => (
+  <svg width="2em" height="2em" viewBox="0 0 192 192" {...props}>
+    <g
+      fill="none"
+      strokeMiterlimit={10}
+      fontFamily="none"
+      fontWeight="none"
+      fontSize="none"
+      textAnchor="none"
+      style={{
+        mixBlendMode: "normal"
+      }}
+    >
+      <path d="M0 192V0h192v192z" />
+      <path
+        d="M96 16c-6.624 0-12 5.376-12 12v5.563C63.304 38.898 48 57.64 48 80v48l-16 16v8h128v-8l-16-16V80c0-22.36-15.304-41.086-36-46.422V28c0-6.624-5.376-12-12-12zM80 160c0 8.84 7.168 16 16 16s16-7.168 16-16z"
+        fill="#fff"
+      />
+    </g>
+  </svg>
+)
