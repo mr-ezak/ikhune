e14448b98075f5a91472e7c70f6efff2faf4e9de
Mr Ezak <ezaksaeed@gmail.com>
2019-06-05 01:20:37 +0430

    Fix the server side rendering

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,30 @@
 import Document, { Html, Head, Main, NextScript } from "next/document"
-import link from "next/dist/client/link"
+import { ServerStyleSheet } from "styled-components"
 
-class MyDocument extends Document {
+export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
+        })
+
+      const initialProps = await Document.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        )
+      }
+    } finally {
+      sheet.seal()
+    }
   }
 
   render() {
@@ -22,5 +42,3 @@ class MyDocument extends Document {
     )
   }
 }
-
-export default MyDocument
